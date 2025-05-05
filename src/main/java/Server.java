201620eca81daf3ff6f9977f29552a752d0eae10
sapinux <sapinux@gmail.com>
2025-05-05 14:56:29 -0300

    mudanca no valor da variavel port

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,14 +4,13 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
-        int port = 8080;
-
+        int port = 3500;
+        //dsfdsfdfdfsfsf
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             Socket clientSocket = serverSocket.accept();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
